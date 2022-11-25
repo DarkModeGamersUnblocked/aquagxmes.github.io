224963a3e450ac25af2e59bb489ef362926296df
Emerald <115799008+EmeraldGxmes@users.noreply.github.com>
2022-11-24 19:19:06 -0500

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,1 +1,34 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    
+    console.log(document.cookie);
+    var visit = getCookie("cookie");
+    if (visit == null) {
+        document.getElementById("overlay").style.display = "block";
+        var expire = new Date();
+        expire = new Date(expire.getTime() + 7776000000);
+        document.cookie = "cookie=here; expires=" + expire;
+    }
+});
 
+function getCookie(c_name) {
+    var c_value = document.cookie;
+    var c_start = c_value.indexOf(" " + c_name + "=");
+    if (c_start == -1) {
+        c_start = c_value.indexOf(c_name + "=");
+    }
+    if (c_start == -1) {
+        c_value = null;
+    } else {
+        c_start = c_value.indexOf("=", c_start) + 1;
+        var c_end = c_value.indexOf(";", c_start);
+        if (c_end == -1) {
+            c_end = c_value.length;
+        }
+        c_value = unescape(c_value.substring(c_start, c_end));
+    }
+    return c_value;
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
