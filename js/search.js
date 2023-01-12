fc303eaaef1fda5a264bd78eb8b9658e97e1b677
Aqua <115799008+AquaGxmes@users.noreply.github.com>
2023-01-12 06:25:36 -0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -3,12 +3,12 @@ function startSearch() {
   input = document.getElementById("srchbar");
   filter = input.value.toUpperCase();
   ul = document.querySelector(".gamescontainer");
-  for (i = 0; i < li.length; i++) {
+  for (i = 0; i < ul.length; i++) {
     a = ul[i].getElementsByTagName("p")[0];
     if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.position = "relative";
+      ul[i].style.position = "relative";
     } else {
-      li[i].style.position = "none";
+      ul[i].style.position = "none";
     }
   }
 }
