92834fd5d974f720836df46a99f9fcdd287073ab
Aqua <115799008+AquaGxmes@users.noreply.github.com>
2023-01-11 23:00:36 -0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,15 +1,14 @@
 function startSearch() {
-  var input, filter, ul, li, a, i, gf;
+  var input, filter, ul, a, i, gf;
   input = document.getElementById("srchbar");
   filter = input.value.toUpperCase();
-  ul = document.querySelector(".game");
-  li = ul.getElementsByTagName("div");
+  ul = document.querySelector(".gamescontainer");
   for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("p")[0];
+    a = ul[i].getElementsByTagName("p")[0];
     if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
+      li[i].style.position = "relative";
     } else {
-      li[i].style.display = "none";
+      li[i].style.position = "none";
     }
   }
 }
