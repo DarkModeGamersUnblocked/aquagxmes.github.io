288825463d9ce53137566c55981a9237521f0fa2
Aqua <115799008+AquaGxmes@users.noreply.github.com>
2023-01-18 09:14:38 -0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,14 +1,14 @@
-function startSearch() {
-  var input, filter, ul, a, i, gf;
-  input = document.getElementById("srchbar");
-  filter = input.value.toUpperCase();
-  ul = document.querySelector(".gamescontainer");
-  for (i = 0; i < ul.length; i++) {
-    a = ul[i].getElementsByTagName("p")[0];
-    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      ul[i].style.position = "relative";
-    } else {
-      ul[i].style.position = "none";
+const games = document.getElementById("gamescontainer");
+const search = document.getElementById("srchbar");
+function gamefilter() {
+    var filter = search.value.toUpperCase();
+    for (i = 0; i < games.children.length; i++) {
+        var a = games.children[i].getElementsByTagName("p")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            games.children[i].style.display = "";
+        } else {
+            games.children[i].style.display = "none";
+        }
     }
-  }
 }
+search.addEventListener("keyup", gamefilter);
