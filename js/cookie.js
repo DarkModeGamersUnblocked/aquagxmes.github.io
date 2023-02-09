75c04a9e3d8f34e9add14756e5b3e0c0ee06fbb7
Aqua <115799008+AquaGxmes@users.noreply.github.com>
2023-02-09 16:47:08 -0500

    Update cookie.js

--- js/cookie.js
+++ js/cookie.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
-    
+        doc.getElementsByTagName('link')[1].href = ls.getItem("iconcloak"),
+    doc.title = ls.getItem("titlecloak")     
     console.log(document.cookie);
     var visit = getCookie("cookie");
     if (visit == null) {
@@ -26,9 +27,37 @@ function getCookie(c_name) {
         }
         c_value = unescape(c_value.substring(c_start, c_end));
     }
+    
     return c_value;
 }
 
 function off() {
   document.getElementById("overlay").style.display = "none";
+}
+const doc = document
+const ls = localStorage
+function setico(urle) {
+    ls.setItem("iconcloak", urle)
+}
+function setitle(urle) {
+    ls.setItem("titlecloak", urle)
+}
+function toggleSettings() {
+if (doc.getElementById("settings").style.display == "none"){
+    doc.getElementById("settings").style.display = ""
+} else if (doc.getElementById("settings").style.display == ""){
+    doc.getElementById("settings").style.display = "none"
+    }
+}
+function TitleSetting(thing) {
+    setitle(thing),
+    doc.title = ls.getItem("titlecloak")
+}
+function DefaultSetting() {
+    ls.setItem("tabcloak", 'https://aquagxmes.github.io/img/aqua.ico'),
+    doc.getElementsByTagName('link')[1].href = ls.getItem("iconcloak")
+}
+function IconSetting(thing) {
+    setico(thing),
+    doc.getElementsByTagName('link')[1].href = ls.getItem("iconcloak")
 }
