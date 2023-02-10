eca5da26e8b77cc32b5acb8272bea9928f281a75
Aqua <115799008+AquaGxmes@users.noreply.github.com>
2023-02-10 08:59:25 -0500

    Update cookie.js

--- js/cookie.js
+++ js/cookie.js
@@ -34,30 +34,33 @@ function getCookie(c_name) {
 function off() {
   document.getElementById("overlay").style.display = "none";
 }
+           .;                   
 const doc = document
 const ls = localStorage
 function setico(urle) {
-    ls.setItem("iconcloak", urle)
+ls.setItem("iconcloak", urle)
 }
 function setitle(urle) {
-    ls.setItem("titlecloak", urle)
+ls.setItem("titlecloak", urle)
 }
 function toggleSettings() {
 if (doc.getElementById("settings").style.display == "none"){
-    doc.getElementById("settings").style.display = ""
+doc.getElementById("settings").style.display = ""
 } else if (doc.getElementById("settings").style.display == ""){
-    doc.getElementById("settings").style.display = "none"
-    }
+doc.getElementById("settings").style.display = "none"
 }
+}                      
 function TitleSetting(thing) {
-    setitle(thing),
-    doc.title = ls.getItem("titlecloak")
+setitle(thing),
+doc.title = ls.getItem("titlecloak")
 }
 function DefaultSetting() {
-    ls.setItem("tabcloak", 'https://aquagxmes.github.io/img/aqua.ico'),
-    doc.getElementsByTagName('link')[1].href = ls.getItem("iconcloak")
+setico('https://aquagxmes.github.io/img/aqua.ico'),
+setitle('Aqua Games')
+doc.getElementsByTagName('link')[1].href = ls.getItem("iconcloak")
+doc.title = ls.getItem("titlecloak")
 }
 function IconSetting(thing) {
-    setico(thing),
-    doc.getElementsByTagName('link')[1].href = ls.getItem("iconcloak")
+setico(thing),
+doc.getElementsByTagName('link')[1].href = ls.getItem("iconcloak")
 }
