b8f01b874d0b9a4e957355a0270367b9825aa91d
Aqua <115799008+AquaGxmes@users.noreply.github.com>
2023-02-22 11:19:09 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -2,13 +2,15 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const gamePage = urlParams.get('game');
 const gameIframe = document.getElementById("aquagame");
-const title = document.getElementById('');
-const author = document.getElementById('controlsinfo');
+const title = document.getElementById('GmeTitle');
+const author = document.getElementById('GmeAuthor');
 
 window.addEventListener('load', async() => {
- jso = await fetch('/js/games.json');
+ jso = await fetch('/js/game.json');
   gms = await jso.json();
   const dat = gms[gamePage]
   gameIframe.src = gms.iframe
-  
+  title.InnerHTML = gms.Title
+ author.InnerHTML = gms.Author
+  document.getElementById("OrgTitleName").InnerHTML = gms.PageTitle
 })
